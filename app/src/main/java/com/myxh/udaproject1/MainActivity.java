630e2a93ee2f93cf92e85ff088e05ce301f491f7
myxh <myxhchen@gmail.com>
2016-08-29 10:49:19 +0800

    p0

--- app/src/main/java/com/myxh/udaproject1/MainActivity.java
+++ app/src/main/java/com/myxh/udaproject1/MainActivity.java
@@ -2,6 +2,8 @@ package com.myxh.udaproject1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToastPopularMovie(View view) {
+        Toast.makeText(this,"Popular Movie Message",Toast.LENGTH_SHORT).show();
     }
 }
